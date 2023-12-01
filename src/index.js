56566ba416a1aa576412f9f63d590e8e114e56fc
Stuart Bate <bate.stuart93@gmail.com>
2023-12-01 13:12:54 -0800

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,29 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 
-  const newTaskForm = document.getElementById("create-task-form");
-  const newTaskDescription = document.getElementById("new-task-description");
-  const newTaskPriority = document.getElementById("new-task-priority");
+  const newTaskForm = document.querySelector('form');
 
-  //ul where new tasks will live on the DOM
-  const newTaskUl = document.getElementById("tasks");
+  //attach a submit event listener to newTaskForm.
+  newTaskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //create a variable called newToDo that holds the value of the targeted input.
+    const newToDo = e.target.querySelector('#new-task-description').value
 
-  //attach event listeners
-  newTaskForm.addEventListener("submit", () =>
-    createNewTask(event, newTaskDescription)
-  );
+    //pass in the newToDo variable as an argument to buildToDo. 
+    buildToDo(newToDo);
+
+    //after buildToDo function runs, reset the newTaskForm so the input is empty
+    newTaskForm.reset();
+  })
 });
 
-const createNewTask = (event, newTaskDescription) => {
-  //stop form from trying to submit
-  event.preventDefault();
+  //when user submits, we pass in the targeted input into buildToDo function
+  function buildToDo(newToDo){
+    //first, the buildToDo function creates an li element and a button element. 
+    const taskLi = document.createElement('li');
+    const deleteButton = document.createElement('button');
 
-  const newTask = document.createElement("li");
-  newTask.innerText = newTaskDescription.value;
+    //next, we set the textContent of the btn element to 'x'.
+    deleteButton.textContent = 'x';
 
-  appendNewTask(newTask);
-};
+    //after, we set the textContent of the li element to the todo parameter.
+    //remember, when we submit, we are passing in targeted input into the parameters. 
+    taskLi.textContent = newToDo;
 
-const appendNewTask = task => {
-  document.getElementById("tasks").appendChild(task);
-};
+    //next, we append the new btn to the li.
+    taskLi.appendChild(deleteButton);
+
+    //then, we add the li element to the ul element with id of tasks
+    document.querySelector('#tasks').appendChild(taskLi);
+  
+  
+    //finally, we attach a click event listener to the btn which invokes the handleDelete function
+    deleteButton.addEventListener('click', handleDelete);
+  }
+
+  //when the button element with the textContent of 'x' is clicked, this function will run.
+  function handleDelete(e){
+    //this function targets the parent node of the button and removes it.
+    //in this case, the parentNode of the button element is the li element, so the li is removed from the list.
+    e.target.parentNode.remove();
+  }
